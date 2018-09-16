b9bf96401db82143c3abc270a5d02bbb947a15a8
EditB <beditto@yahoo.com>
2018-09-16 21:22:29 +1000

    First draft

---
+++ search.js
@@ -0,0 +1,29 @@
+//console.log("test");
+
+//Note: implement commander/inquirer to read in command line instructions
+
+const jq = require('node-jq');
+
+// Note: split the below into a function, with parameters to pass filter, jsonPath and options
+
+//const filter = '.[] | ._id';
+//const filter = '.[] | select(.name == "Enthaze")';
+const filter = '.[] | select(._id == 101)';
+const jsonPath = './data/organizations.json';
+const options = {};
+
+jq.run(filter, jsonPath, options)
+  .then((output) => {
+    //const newObj = output;
+    const newObj = JSON.parse(output);
+    //console.log(" new object: ");
+    //Note: it is a string!
+    //console.log(typeof newObj);
+    //console.log(newObj._id);
+    console.log(output);
+    //Note: here we can do another jq.run with the foreign key if there are related entities.
+  })
+  .catch((err) => {
+    console.error(err);
+    // Something went wrong...
+  })
